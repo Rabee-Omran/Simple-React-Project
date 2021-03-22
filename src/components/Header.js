dc0cf0e64d452a374acd26f423f48db6030e312d
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-22 14:28:35 +0200

    add inline style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
   return (
     <div>  
-   <h1>Hello from Header</h1>
+   <h1 style ={{color: "red",fontSize:"100px"}}>Hello from Header</h1>
     </div>
 
   );
