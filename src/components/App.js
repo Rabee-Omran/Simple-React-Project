b7a00fcd20a8dc5b32770dae2cbbd65cf5fbc030
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-24 10:47:42 +0200

    use state --simple

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
-import File from './File';
-// function App(){
-//   return (
-//     <div className= "container">  
-//   test
-//     </div>
-
-//   );
-
-// }
-
-// export default App;
 
 class App extends Component{
+
+  constructor(){
+    super()
+    this.state ={answer:'YES'}
+
+  }
 
   render(){ 
     
     return (
     <div className= "container">  
-       <File name='rabee' name2= 'rabee2'/>
-       <File name='rabee' name2= 'rabee2'/>
-       <File name='rabee' name2= 'rabee2'/>
-       <File name='rabee' name2= 'rabee2'/>
+     <h1>do you love React ?? {this.state.answer}</h1>
     </div>
 
   );}
