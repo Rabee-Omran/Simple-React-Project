4dbe2413fa2354bcb6bf485374b60881834330cd
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-24 15:24:40 +0200

    using event , setState

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import MyCard from './MyCard';
 
 
 class App extends Component{
 
-  
+  constructor(){
+    super()
+    this.state = {
+      number : 0
+    }
+
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+
+ 
+  clickHandler(){
+    this.setState((prevState) =>  {return{ number:prevState.number +1}})
+    
+  }
+
   render(){
+
   return (
 
-    <div className= "container">  
+    <div className= "container text-center mt-5">  
    
-         <MyCard/>
+        <h1>{this.state.number}</h1>
+        <button className='btn btn-success' onClick={this.clickHandler}>CLICK ME !</button>
     
     </div>
 
